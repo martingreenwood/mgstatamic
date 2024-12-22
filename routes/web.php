139fd869f55af02ded2bb56986f29f0477f4a45c
Martin Greenwood <martin.greenwood@baronseden.com>
2024-12-22 09:01:00 +0000

    added printful api endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+Route::get('/printful/products', function () {
+    $response = Http::withHeaders([
+        'Authorization' => 'Bearer ' . env('PRINTFUL_API_KEY'),
+        'X-PF-Language' => 'en_GB',
+    ])->get('https://api.printful.com/products');
+
+    return response()->json($response->json(), $response->status());
+});
+
+Route::get('/printful/products/{id}', function ($id) {
+    $response = Http::withHeaders([
+        'Authorization' => 'Bearer ' . env('PRINTFUL_API_KEY'),
+        'X-PF-Language' => 'en_GB',
+    ])->get("https://api.printful.com/products/{$id}");
+
+    return response()->json($response->json(), $response->status());
+});
